bdf9f8ede9cc1461699c8f1acf5a46b447d83bc5
Michael Rawlings <rawlingsm99@gmail.com>
2022-05-07 20:27:00 -0500

    project complteted

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ btns.forEach(function (btn) {
     if (styles.contains("decrease")) {
       count--;
     }
+    else if(styles.contains("increase")){
+      count++; 
+    }
+    else{
+       count = 0;
+    }  
+     
     value.textContent = count; 
+    
+    
+    
+if(count > 0){
+  value.style.color = "green";
+}
+if(count < 0) {
+  value.style.color = "red"
+}
+if (count === 0) {
+  value.style.color = "black"
+}
   });
 });
+ 
